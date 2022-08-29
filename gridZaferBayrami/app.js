541530f950427d4808e20ebf438e351c3907dc27
Cooper <dancersoftaranto@gmail.com>
2022-08-29 16:52:02 +0300

    alert mesajı değişikliği

--- gridZaferBayrami/app.js
+++ gridZaferBayrami/app.js
@@ -1,3 +1,3 @@
 alert(
-  "Bu vesile ile 30 Ağustos tarihinden geçerli olmak üzere bir üst rütbeye terfi eden bütün silah arkadaşlarımın yeni rütbelerinin hayırlı olmasını dilerim. Milletimizin bize emaneti olan şanlı üniformalarımızı en yakın zamanda tekrar giyip görevimizin başında buluşmak üzere... Cooper"
+  "30 Ağustos Zafer Bayramınız Kutlu Olsun... Cooper"
 );
