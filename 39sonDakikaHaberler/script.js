9e3258565b93d8484f0852d9c281e92329d1d2eb
Cooper <dancersoftaranto@gmail.com>
2022-09-26 17:26:04 +0300

    ülke kodları eklendi

--- 39sonDakikaHaberler/script.js
+++ 39sonDakikaHaberler/script.js
@@ -27,7 +27,7 @@ const news = (data) => {
   });
 };
 
-//? country Possible options:
+//? country Possible options (ülke değiştirmek için):
 //* ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za.
 
 window.addEventListener("load", newsQuerry("tr"));
